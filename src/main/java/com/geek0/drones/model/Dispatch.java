63a46e81a02c110f74e2798b8ebb042228043661
Uthman Bakar <uthmanb@outlook.com>
2022-09-27 23:14:51 +0100

    Dispatch document omitted in last commit

--- src/main/java/com/geek0/drones/model/Dispatch.java
+++ src/main/java/com/geek0/drones/model/Dispatch.java
@@ -1,10 +1,12 @@
 package com.geek0.drones.model;
 
+import com.geek0.drones.enums.State;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 //import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -12,13 +14,35 @@ import java.util.List;
 public class Dispatch {
     @Id
     private String id;
-//    @DocumentReference
     private String droneId;
-//    @DocumentReference
     private List<String> medicationIds;
+    private State state;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public Dispatch(String droneId, List<String> medicationIds) {
         this.droneId = droneId;
         this.medicationIds = medicationIds;
     }
+
+    public Dispatch(String droneId, List<String> medicationIds, State state, LocalDateTime createdAt) {
+        this.droneId = droneId;
+        this.medicationIds = medicationIds;
+        this.state = state;
+        this.createdAt = createdAt;
+    }
+
+    public Dispatch(String droneId, List<String> medicationIds, State state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.droneId = droneId;
+        this.medicationIds = medicationIds;
+        this.state = state;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    /**TODO: Dispatches should represent a log
+     * Add created and updated times
+     * Add State
+     */
+
 }
